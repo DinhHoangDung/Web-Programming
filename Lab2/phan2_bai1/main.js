4c748b65a62bdae646e08dd2292881aff47df9ff
Hoang Dung <dhdung71@gmail.com>
2023-10-23 09:35:02 +0700

    finish

--- Lab2/phan2_bai1/main.js
+++ Lab2/phan2_bai1/main.js
@@ -17,7 +17,7 @@ function createTable()
             tagColumn.classList.add('border');
             tagColumn.classList.add('p-4');
             tagRow.appendChild(tagColumn);
-        }
+        } 
     }
     table.appendChild(tagTable);
 }
@@ -41,20 +41,20 @@ function addRow()
   
     if (table.childNodes.length == 1)
     {
-      alert("Create table để thực hiện thao tác này");
+      alert("Create table to do this!");
       return;
     }
   
     var tagTable = table.firstElementChild;
     if (tagTable.childNodes[0] == null)
     {
-      alert("Create new table để thực hiện thao tác này");
+      alert("Create table to do this!");
       return;
     }
     var countCol = tagTable.childNodes[0].childNodes.length;
     if (countCol == 0)
     {
-      alert("Create new table để thực hiện thao tác này");
+      alert("Create table to do this!");
       return;
     }
     var tagRow = document.createElement("tr");
@@ -74,19 +74,19 @@ function addCol()
     var table = document.getElementById("tab");
   
     if (table.childNodes.length == 1){
-      alert("Create table để thực hiện thao tác này");
+      alert("Create table to do this!");
       return;
     }
     var tagTable = table.firstElementChild;
     if (tagTable.childNodes[0] == null)
     {
-        alert("Create new table để thực hiện thao tác này");
+        alert("Create table to do this!");
         return;
     }
     var countRow = tagTable.childNodes.length;
     if (countRow == 0)
     {
-      alert("Create new table để thực hiện thao tác này");
+      alert("Create table to do this!");
       return;
     }
     for (var i = 0; i < countRow; i++)
@@ -97,4 +97,57 @@ function addCol()
       tagColumn.classList.add('p-4');
       tagRow.appendChild(tagColumn);
     }
-  }
+}
+
+function removeRow()
+{
+    var table = document.getElementById("tab");
+    if (table.childNodes.length == 1)
+    {
+      alert("Create table to do this!");
+      return;
+    }
+    var tagTable = table.firstElementChild;
+    var countRow = tagTable.childNodes.length;
+    var Rownumber = document.getElementById("Rownumber").value;
+    if (!Rownumber)
+    {
+      alert("Input index of row!");
+      return;
+    }
+    if (Rownumber >= countRow || Rownumber < 0)
+    {
+      alert("Invalid input index!");
+      return;
+    }
+
+    tagTable.removeChild(tagTable.childNodes[Rownumber]);
+}
+function removeCol()
+{
+    var table = document.getElementById("tab");
+    if (table.childNodes.length == 1)
+    {
+      alert("Create table to do this!");
+      return;
+    }
+    var tagTable = table.firstElementChild;
+    var Colnumber = document.getElementById("Colnumber").value;
+    var countCol = tagTable.childNodes[0].childNodes.length;
+    var countRow = tagTable.childNodes.length;
+    if (!Colnumber)
+    {
+      alert("Input index of column!");
+      return;
+    }
+    if (Colnumber >= countCol || Colnumber < 0)
+    {
+      alert("Invalid input index!");
+      return;
+    }
+    for (var i = 0; i < countRow; i++)
+    {
+      var tagRow = tagTable.childNodes[i];
+      tagRow.removeChild(tagRow.childNodes[Colnumber]);
+    }
+}
