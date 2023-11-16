2c1a34ab8125033d006f292b11afd6df18a45ce5
Hoang Dung <dhdung71@gmail.com>
2023-11-16 23:18:57 +0700

    phan1_bai3

---
+++ Lab4/phan1_bai3/login.php
@@ -0,0 +1,51 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">    
+</head>
+
+<body>
+    <h4>username: admin, password: 123</h4>
+    <div class="container bg-light mt-3" style="width: 400px">
+        <div class="row">
+            <h3 class="fw-bold text-center mt-2">Login</h3>
+        </div>
+
+        <form action="login.php" method="post">
+            
+            <input type="text" class="form-control mt-3" name="username" placeholder="Username">
+            <input type="password" class="form-control mt-3" name="password" placeholder="Password">
+            <div class = "text-center">
+                <input type="submit" class="form btn btn-outline-primary mt-3" name="submit" value="Login">
+            </div>
+        </form>
+    </div>
+    <?php
+        if(isset($_SESSION['username'])) 
+        {
+            header('Location: info.php');
+        }
+        if (isset($_POST['submit'])) 
+        {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            if ($username == 'admin' && $password == '123') 
+            {
+                $_SESSION['username'] = $username;
+                header('location: info.php');
+            }
+            else
+            {
+                echo "<script>alert('Invalid username or password')</script>";
+            }
+        }
+    ?>
+</body>
---
+++ Lab4/phan1_bai3/logout.php
@@ -0,0 +1,26 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">    
+
+</head>
+<body>
+    <?php
+        session_destroy();
+        echo "<script>alert('Logout success!')</script>";
+    ?>
+    <div class="container" style="width: 400px;">
+    <div class="row">
+        <button class="btn btn-primary mt-3" onclick="location.href='login.php'">Login</button>
+    </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+</html>
